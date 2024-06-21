0c2c630628c0d470f808251f7129fd2f2228dc35
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2024-06-21 15:24:55 +0300

    added user option list

--- js/login.js
+++ js/login.js
@@ -1,5 +1,20 @@
 const loginForm = document.getElementById("login-form");
-loginForm.addEventListener("submit", handleLogin);
+if (loginForm) {
+  loginForm.addEventListener("submit", handleLogin);
+}
+
+const logoutOption = document.getElementById("logout-option");
+if (logoutOption) {
+  logoutOption.addEventListener("click", handleLogout);
+}
+
+const userIcon = document.querySelector(".user-icon-container")
+if (userIcon) {
+  if (isLoggedIn()) {
+    userIcon.classList.add("user-icon-container-active")
+  }
+  userIcon.addEventListener("click", showUserOptionsList)
+}
 
 function handleLogin(event) {
   event.preventDefault();
@@ -9,7 +24,7 @@ function handleLogin(event) {
   let users = localStorage.getItem("users");
   if (users) {
     users = JSON.parse(users);
-  
+
     for (user of users) {
       if (user.email == email && user.password == password) {
         addToCookies("email", email);
@@ -22,15 +37,11 @@ function handleLogin(event) {
   showError("Email or password is incorrect");
 }
 
-const logoutForm = document.getElementById("logout-form");
-logoutForm.addEventListener("submit", handleLogout);
-
 function handleLogout(event) {
-    event.preventDefault();
-    removeFromCookies("email");
-    location.assign("./login.html");
+  event.preventDefault();
+  removeFromCookies("email");
+  location.assign("./login.html");
 }
-
 
 function showError(msg) {
   const errorContainer = document.getElementById("error-container");
@@ -40,11 +51,38 @@ function showError(msg) {
 function addToCookies(key, value) {
   let date = new Date();
   date.setDate(date.getDate() + 1);
-  document.cookie = `${key}=${value};expire=${date}`;
+  document.cookie = `${key}=${value};expires=${date}`;
 }
 
 function removeFromCookies(key, value) {
   let date = new Date();
   date.setDate(date.getDate() - 1);
-  document.cookie = `${key}=${value};expire=${date}`;
+  document.cookie = `${key}=${value};expires=${date}`;
+}
+
+function isLoggedIn() {
+  try {
+    let cookies = document.cookie.split(";");
+    for (let c of cookies) {
+      c = c.split("=");
+      if (c[0] == "email" && c[1]) {
+        return true;
+      }
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
+}
+
+function showUserOptionsList(event) {
+  let userOptions = document.getElementsByClassName("user-options")[0]
+  userOptions.classList.toggle("show-user-options")
+  if(isLoggedIn()) {
+    document.getElementById("login-option").style.display = "none"
+    document.getElementById("signup-option").style.display = "none"
+  }
+  else {
+    document.getElementById("logout-option").style.display = "none"
+  }
 }
