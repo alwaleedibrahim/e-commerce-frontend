ea6cb24759445522e5ccde80e6c6f153aed2200d
Menna Elbadry <mennatallah701@gmail.com>
2024-06-22 16:49:30 +0300

    update cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -6,48 +6,8 @@ function requestProducts() {
     xhr.onload = function() {
         if (this.status >= 200 && this.status < 300) {
             let products = JSON.parse(this.responseText);
-            let productContainer = document.querySelector('.cart-items');
             let savedCart = JSON.parse(localStorage.getItem('cart')) || [];
-
-            products.forEach(product => {
-                let cartItemElement = document.createElement('div');
-                cartItemElement.classList.add('cart-item', 'cart-row');
-
-                cartItemElement.innerHTML = `
-                    <img src="${product.image}">
-                    <div class="cart-item-details">
-                        <h4>${product.name}</h4>
-                        <p>$${product.price}</p>
-                        <input type="number" value="1" min="1" max="20">
-                        <p class="subtotal">$${product.price}</p>
-                        <button class="remove-item">Remove</button>
-                    </div>`;
-
-              
-                productContainer.appendChild(cartItemElement);
-
-                
-                let quantityInput = cartItemElement.querySelector('input[type="number"]');
-                quantityInput.addEventListener('change', function() {
-                    updateSubtotal(cartItemElement, product);
-                    updateCartTotal();
-                });
-
-                let removeButton = cartItemElement.querySelector('.remove-item');
-                removeButton.addEventListener('click', function() {
-                    cartItemElement.remove();
-                    updateCartTotal();
-                });
-
-               
-                let savedItem = savedCart.find(item => item.productName === product.name);
-                if (savedItem) {
-                    quantityInput.value = savedItem.quantity;
-                    updateSubtotal(cartItemElement, product);
-                }
-            });
-
-            updateCartTotal();
+            createCard(savedCart)
         } else {
             console.error('Failed to load products');
         }
@@ -56,6 +16,81 @@ function requestProducts() {
     xhr.send();
 }
 
+let productContainer = document.getElementById("items");
+
+function currentQuantity(product) {
+    let productAdded = JSON.parse(localStorage.getItem('cart')) || [];
+    let totalQuantity = 0;
+
+    if (product) {
+        let productName = product.name;
+        totalQuantity = productAdded.reduce((total, prod) => {
+            if (prod.name === productName) {
+                return total + 1;
+            }
+            return total;
+        }, 0);
+    } else {
+        totalQuantity = 0;
+    }
+
+    defaultQuantity = totalQuantity;
+    return totalQuantity;
+}
+function createCard(data){
+    data.forEach(product => {
+        let defaultQuantity = currentQuantity(product);
+        console.log(defaultQuantity);
+        let cartItemElement = document.createElement('div');
+        cartItemElement.classList.add('cart-item', 'cart-row');
+        cartItemElement.className ="item"
+
+        const words = product.name.trim().split(/\s+/);
+        const truncatedText = words.slice(0, 3).join(" ");
+
+        cartItemElement.innerHTML = `
+            <div class="item-spacing"> 
+                <img src="${product.image_key}">
+            </div
+            <div class="item-spacing"> 
+                <h4>${truncatedText}</h4>
+            </div>
+            <input type="number" value="${defaultQuantity}" min="1" max="20">
+            <div class="item-spacing"> 
+                <p>$${product.price}</p>
+            </div>
+                <button class="remove-item">Remove</button>
+            `;
+        //     <div class="cart-item-details">
+                
+        //     <p class="subtotal">$${product.price}</p>
+        // </div>`;
+
+        productContainer.appendChild(cartItemElement);
+
+        
+        let quantityInput = cartItemElement.querySelector('input[type="number"]');
+        quantityInput.addEventListener('change', function() {
+            updateSubtotal(cartItemElement, product);
+            updateCartTotal();
+        });
+
+        let removeButton = cartItemElement.querySelector('.remove-item');
+        removeButton.addEventListener('click', function() {
+            cartItemElement.remove();
+            updateCartTotal();
+        });
+
+       
+        let savedItem = savedCart.find(item => item.productName === product.name);
+        if (savedItem) {
+            quantityInput.value = savedItem.quantity;
+            updateSubtotal(cartItemElement, product);
+        }
+    });
+
+    updateCartTotal();
+}
 function updateSubtotal(cartItemElement, product) {
     let quantity = parseInt(cartItemElement.querySelector('input[type="number"]').value);
     let subtotal = quantity * product.price;
